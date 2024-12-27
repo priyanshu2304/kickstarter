56471385f1811e6900d5423d7137aa1b5d597cd8
priyanshu2304 <priyanshumodi1998@gmail.com>
2024-12-27 10:04:12 +0530

    added listing page, sorting functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
-
+import '@/styles/globals.css';
+import Header from '@/components/Header';
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
